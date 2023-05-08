8a6df755e7dcca72637fc2933080473d77ba4e74
Lam Nguyen Huy Hoang <huyyhoangg150203@hotmail.com>
2023-05-08 19:01:48 +0700

    add circle shape

---
+++ DoubleBufferedPanel.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace GraphicEx
+{
+    public class DoubleBufferedPanel : Panel
+    {
+        public DoubleBufferedPanel()
+        {
+            this.DoubleBuffered = true;
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);    
+            UpdateStyles();
+        }
+    }
+}
---
+++ Shaping/CircleShape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+
+namespace GraphicEx
+{
+    public class CircleShape : Shape, IFillable
+    {
+        
+        private bool fill = false;
+        public CircleShape(Graphics graphics, bool fill = false) : base(graphics)
+        {
+            setFill(fill);
+        }
+
+        public override void generateBound()
+        {
+            bounds.X = started.X < ended.X ? started.X : ended.X;
+            bounds.Y = started.Y < ended.Y ? started.Y : ended.Y;
+            int a = Math.Abs(ended.X - started.X);
+            int b = Math.Abs(ended.Y - started.Y);
+            bounds.Width = Math.Max(a, b);
+            bounds.Height = Math.Max(a, b);
+            ended.X = (int)(bounds.X + bounds.Width);
+            ended.Y = (int)(bounds.Y + bounds.Height);
+        }
+        
+        public override void Draw()
+        {
+            if (fill) graphics.FillEllipse(tools.brush, bounds);
+            graphics.DrawEllipse(tools.pen, bounds);
+        }
+
+        public override void finishDraw()
+        {
+            path.AddEllipse(bounds);
+        }
+
+
+        public void setFill(bool fill)
+        {
+            this.fill = fill;
+        }
+    }
+}
